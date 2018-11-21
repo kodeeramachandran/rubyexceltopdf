d677ac60aa696042961133959194da63325199b7
kotee <koteeswaran.ramachandran@sixt.com>
2018-11-21 10:09:16 +0100

    resforce changes

--- app/controllers/converts_controller.rb
+++ app/controllers/converts_controller.rb
@@ -14,14 +14,14 @@ class ConvertsController < ApplicationController
     path = File.join Rails.root, 'public'
     fileName = params['File Name'];
 
-    File.open("#{Rails.root}/public/#{fileName}.xlsx", 'wb') do |f|
-     f.write(params['Xlsx Body'])
-    end 
+    #File.open("#{Rails.root}/public/#{fileName}.xlsx", 'wb') do |f|
+     #f.write(params['Xlsx Body'])
+    #end 
 
   
   #download_file_path = "#{Rails.root}/public/file_conversion/#{fileName}.pdf"
   
-  connectsalesforce(params['ParentId'],fileName);
+ # connectsalesforce(params['ParentId'],fileName);
   render json: {recived: true}, status: :created, location: "Done"
   end
   def connectsalesforce(id,fname)   
