6b40ff91968a1c6d63c904710755f34a7d9b09c5
kotee <koteeswaran.ramachandran@sixt.com>
2018-11-21 17:48:27 +0100

    resforce changes

--- app/controllers/converts_controller.rb
+++ app/controllers/converts_controller.rb
@@ -22,7 +22,7 @@ class ConvertsController < ApplicationController
     attachment = client.query("select Id, Name, Body from Attachment Where Id ="+ "'"+id+"'").first
     File.open("#{Rails.root}/public/#{attachment.Name}", 'wb') { |f| f.write(attachment.Body) } 
 
-    render json: {recived: true}, status: :created, location: "Done",action: connectsalesforce(params['ParentId'],attachment.Name,client)
+    render json: {recived: true,name: attachment.Name}, status: :created, location: "Done"
   end
   def connectsalesforce(id,fname,client) 
 
