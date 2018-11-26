7c8406a1fc97f9e9af373b0c3aab86d95a17f3a8
kotee <koteeswaran.ramachandran@sixt.com>
2018-11-26 10:29:15 +0100

    updated to through http call

--- app/controllers/converts_controller.rb
+++ app/controllers/converts_controller.rb
@@ -1,4 +1,5 @@
 require_dependency 'libreconv'
+require 'json'
 class ConvertsController < ApplicationController
  skip_before_action :verify_authenticity_token  
   # GET /converts
@@ -12,14 +13,14 @@ class ConvertsController < ApplicationController
   def create 
     header = {'Content-Type' =>'application/json','Authorization' => 'OAuth '+params['sessionId']}
     id = params['AttachmentId']
-    uri = URI.parse("https://sixt--salesdev--c.cs105.visual.force.com/services/data/v32.0/sobjects/Attachment/"+id+"?fields=Name,Body")
+    uri = URI.parse("https://sixt--salesdev--c.cs105.visual.force.com/services/data/v32.0/sobjects/Attachment/"+id+"/Body")
     https = Net::HTTP.new(uri.host,uri.port)
     https.use_ssl = true
     req = Net::HTTP::Get.new(uri.path, header)
     attachment = https.request(req)
     
-    File.open("#{Rails.root}/public/#{attachment.Name}", 'wb') { |f| f.write(attachment.Body) } 
-    connectsalesforce(params['ParentId'],attachment.Name,params['sessionId'])
+    File.open("#{Rails.root}/public/#{params['Name']}", 'wb') { |f| f.write(attachment.body) } 
+    connectsalesforce(params['ParentId'],params['Name'],params['sessionId'])
     render json: {convertDone: true}, status: :created, location: "Done"
   end
   def connectsalesforce(id,fname,sessionId)
