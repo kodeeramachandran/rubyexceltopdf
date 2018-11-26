67a8025a8a1759c7c4c34a1c565c2d9f89381340
kotee <koteeswaran.ramachandran@sixt.com>
2018-11-26 10:39:29 +0100

    updated to through http call url change

--- app/controllers/converts_controller.rb
+++ app/controllers/converts_controller.rb
@@ -13,23 +13,24 @@ class ConvertsController < ApplicationController
   def create 
     header = {'Content-Type' =>'application/json','Authorization' => 'OAuth '+params['sessionId']}
     id = params['AttachmentId']
-    uri = URI.parse("https://sixt--salesdev--c.cs105.visual.force.com/services/data/v32.0/sobjects/Attachment/"+id+"/Body")
+    baseURL = params['url']
+    uri = URI.parse(baseURL+"/services/data/v32.0/sobjects/Attachment/"+id+"/Body")
     https = Net::HTTP.new(uri.host,uri.port)
     https.use_ssl = true
     req = Net::HTTP::Get.new(uri.path, header)
     attachment = https.request(req)
     
     File.open("#{Rails.root}/public/#{params['Name']}", 'wb') { |f| f.write(attachment.body) } 
-    connectsalesforce(params['ParentId'],params['Name'],params['sessionId'])
+    convertandCreateAttachment(params['ParentId'],params['Name'],params['sessionId'],baseURL)
     render json: {convertDone: true}, status: :created, location: "Done"
   end
-  def connectsalesforce(id,fname,sessionId)
+  def convertandCreateAttachment(id,fname,sessionId, baseURL)
     pdfname = fname.gsub 'xlsx', 'pdf'
     %x("#{Rails.root}/public/office/program/swriter" --headless --convert-to pdf --outdir  "#{Rails.root}/public/file_conversion/" "#{Rails.root}/public/#{fname}")          
 
     header = {'Content-Type' =>'application/json','Authorization' => 'OAuth '+sessionId}
     data = {"ParentId" => id,"Description" => "Convert document","Name" => pdfname, "Body" => Base64::encode64(File.read("#{Rails.root}/public/file_conversion/#{pdfname}"))}
-    uri = URI.parse("https://sixt--salesdev--c.cs105.visual.force.com/services/data/v32.0/sobjects/Attachment/")
+    uri = URI.parse(baseURL+"/services/data/v32.0/sobjects/Attachment/")
     https = Net::HTTP.new(uri.host,uri.port)
     https.use_ssl = true
     req = Net::HTTP::Post.new(uri.path, header)
